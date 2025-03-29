602f2ed64414ce10b4f71928655e12dcaabb6d78
CaptainMentallic <captainmentallic@outlook.com>
2025-03-28 23:22:32 -0600

    Add project files.

---
+++ WindowsOptimizer2/Forms/MainForm.Designer.cs
@@ -0,0 +1,426 @@
+﻿namespace WindowsOptimizer2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            tabControl1 = new TabControl();
+            Home = new TabPage();
+            splitContainer1 = new SplitContainer();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            pictureBox1 = new PictureBox();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            label3 = new Label();
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label2 = new Label();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            Performance = new TabPage();
+            Cleanup = new TabPage();
+            Gaming = new TabPage();
+            Network = new TabPage();
+            Settings = new TabPage();
+            button16 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button15 = new Button();
+            tabControl1.SuspendLayout();
+            Home.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(Home);
+            tabControl1.Controls.Add(Performance);
+            tabControl1.Controls.Add(Cleanup);
+            tabControl1.Controls.Add(Gaming);
+            tabControl1.Controls.Add(Network);
+            tabControl1.Controls.Add(Settings);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1269, 686);
+            tabControl1.TabIndex = 0;
+            // 
+            // Home
+            // 
+            Home.Controls.Add(splitContainer1);
+            Home.Location = new Point(4, 24);
+            Home.Name = "Home";
+            Home.Size = new Size(1261, 658);
+            Home.TabIndex = 0;
+            Home.Text = "Home";
+            Home.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(flowLayoutPanel3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Panel2.Controls.Add(flowLayoutPanel1);
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(flowLayoutPanel2);
+            splitContainer1.Size = new Size(1261, 658);
+            splitContainer1.SplitterDistance = 205;
+            splitContainer1.TabIndex = 0;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.BackColor = Color.WhiteSmoke;
+            flowLayoutPanel3.Controls.Add(pictureBox1);
+            flowLayoutPanel3.Controls.Add(button5);
+            flowLayoutPanel3.Controls.Add(button6);
+            flowLayoutPanel3.Controls.Add(button7);
+            flowLayoutPanel3.Controls.Add(button8);
+            flowLayoutPanel3.Controls.Add(button9);
+            flowLayoutPanel3.Controls.Add(button10);
+            flowLayoutPanel3.Controls.Add(button11);
+            flowLayoutPanel3.Controls.Add(button12);
+            flowLayoutPanel3.Controls.Add(button13);
+            flowLayoutPanel3.Controls.Add(button14);
+            flowLayoutPanel3.Controls.Add(label3);
+            flowLayoutPanel3.Dock = DockStyle.Fill;
+            flowLayoutPanel3.Location = new Point(0, 0);
+            flowLayoutPanel3.MinimumSize = new Size(195, 15);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(205, 658);
+            flowLayoutPanel3.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(202, 138);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 20;
+            pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 147);
+            button5.Name = "button5";
+            button5.Size = new Size(195, 37);
+            button5.TabIndex = 2;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 190);
+            button6.Name = "button6";
+            button6.Size = new Size(195, 37);
+            button6.TabIndex = 11;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(3, 233);
+            button7.Name = "button7";
+            button7.Size = new Size(195, 37);
+            button7.TabIndex = 12;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(3, 276);
+            button8.Name = "button8";
+            button8.Size = new Size(195, 37);
+            button8.TabIndex = 13;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(3, 319);
+            button9.Name = "button9";
+            button9.Size = new Size(195, 37);
+            button9.TabIndex = 14;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(3, 362);
+            button10.Name = "button10";
+            button10.Size = new Size(195, 37);
+            button10.TabIndex = 15;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(3, 405);
+            button11.Name = "button11";
+            button11.Size = new Size(195, 37);
+            button11.TabIndex = 16;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(3, 448);
+            button12.Name = "button12";
+            button12.Size = new Size(195, 37);
+            button12.TabIndex = 17;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(3, 491);
+            button13.Name = "button13";
+            button13.Size = new Size(195, 37);
+            button13.TabIndex = 18;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(3, 534);
+            button14.Name = "button14";
+            button14.Size = new Size(195, 37);
+            button14.TabIndex = 19;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Location = new Point(3, 574);
+            label3.Name = "label3";
+            label3.Size = new Size(195, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Menu";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(18, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Quick Actions";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = AnchorStyles.Top;
+            flowLayoutPanel1.BackColor = Color.FromArgb(235, 235, 235);
+            flowLayoutPanel1.Controls.Add(button16);
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Controls.Add(button2);
+            flowLayoutPanel1.Controls.Add(button15);
+            flowLayoutPanel1.Location = new Point(18, 70);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1016, 118);
+            flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(18, 208);
+            label2.Name = "label2";
+            label2.Size = new Size(204, 28);
+            label2.TabIndex = 5;
+            label2.Text = "Hardware Information";
+            label2.Click += label2_Click;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.BackColor = Color.FromArgb(235, 235, 235);
+            flowLayoutPanel2.Location = new Point(18, 239);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(1016, 376);
+            flowLayoutPanel2.TabIndex = 4;
+            // 
+            // Performance
+            // 
+            Performance.Location = new Point(4, 24);
+            Performance.Name = "Performance";
+            Performance.Size = new Size(1261, 658);
+            Performance.TabIndex = 5;
+            Performance.Text = "Performance";
+            Performance.UseVisualStyleBackColor = true;
+            // 
+            // Cleanup
+            // 
+            Cleanup.Location = new Point(4, 24);
+            Cleanup.Name = "Cleanup";
+            Cleanup.Size = new Size(1261, 658);
+            Cleanup.TabIndex = 1;
+            Cleanup.Text = "Cleanup";
+            Cleanup.UseVisualStyleBackColor = true;
+            // 
+            // Gaming
+            // 
+            Gaming.Location = new Point(4, 24);
+            Gaming.Name = "Gaming";
+            Gaming.Size = new Size(1261, 658);
+            Gaming.TabIndex = 2;
+            Gaming.Text = "Gaming";
+            Gaming.UseVisualStyleBackColor = true;
+            // 
+            // Network
+            // 
+            Network.Location = new Point(4, 24);
+            Network.Name = "Network";
+            Network.Size = new Size(1261, 658);
+            Network.TabIndex = 3;
+            Network.Text = "Network";
+            Network.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            Settings.Location = new Point(4, 24);
+            Settings.Name = "Settings";
+            Settings.Size = new Size(1261, 658);
+            Settings.TabIndex = 4;
+            Settings.Text = "Settings";
+            Settings.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(3, 3);
+            button16.Name = "button16";
+            button16.Size = new Size(248, 113);
+            button16.TabIndex = 10;
+            button16.Text = "Toggle Game Mode";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(257, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(248, 113);
+            button1.TabIndex = 11;
+            button1.Text = "Startup Apps";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(511, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(248, 113);
+            button2.TabIndex = 12;
+            button2.Text = "Update/Install Apps";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(765, 3);
+            button15.Name = "button15";
+            button15.Size = new Size(248, 113);
+            button15.TabIndex = 13;
+            button15.Text = "Cleanup Temporary Files";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1269, 686);
+            Controls.Add(tabControl1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "Tweakify";
+            tabControl1.ResumeLayout(false);
+            Home.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            flowLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage Home;
+        private TabPage Cleanup;
+        private TabPage Gaming;
+        private TabPage Network;
+        private TabPage Settings;
+        private SplitContainer splitContainer1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label1;
+        private Label label2;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Button button5;
+        private Label label3;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private TabPage Performance;
+        private PictureBox pictureBox1;
+        private Button button16;
+        private Button button1;
+        private Button button2;
+        private Button button15;
+    }
+}
---
+++ WindowsOptimizer2/Forms/MainForm.cs
@@ -0,0 +1,25 @@
+namespace WindowsOptimizer2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
